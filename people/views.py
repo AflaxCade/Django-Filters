5d486209e54f3a41ba3af5c37f4ef53f5090726f
AflaxCade <aflah.cade@gmail.com>
2024-08-31 07:28:44 +0300

    implemented filtering functionality

--- people/views.py
+++ people/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from .filters import PersonFilter
 from .models import Person
 
 # Create your views here.
 
 def index(request):
     persons = Person.objects.all()
+    filter = PersonFilter(request.GET, queryset=persons)
+    persons = filter.qs
     context = {
-        'persons': persons
+        'persons': persons,
+        'filter': filter
     }
     return render(request, 'index.html', context)
