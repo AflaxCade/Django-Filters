d589ab8c27e7bc22c7ec598e9a97d8f3d828e8f8
AflaxCade <aflah.cade@gmail.com>
2024-08-31 04:34:28 +0300

    added index view that displays all the data in the Person model

--- people/views.py
+++ people/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Person
 
 # Create your views here.
+
+def index(request):
+    persons = Person.objects.all()
+    context = {
+        'persons': persons
+    }
+    return render(request, 'index.html', context)
