efb393686ceb2fbb4b885b0a750ee99a08fe519c
AflaxCade <aflah.cade@gmail.com>
2024-08-31 04:30:06 +0300

    created Person model class and migrated

--- people/models.py
+++ people/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+
+    sex_choices = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    ]
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=100, choices=sex_choices)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
